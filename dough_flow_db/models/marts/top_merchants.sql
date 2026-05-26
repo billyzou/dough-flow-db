6ec5582561aa5cfc4b89a3a8f257767830ab7406
billyzou <billyzou93@gmail.com>
2026-05-25 17:24:04 -0700

    Exclude Transfers from top_merchants mart

--- dough_flow_db/models/marts/top_merchants.sql
+++ dough_flow_db/models/marts/top_merchants.sql
@@ -5,6 +5,7 @@ select
 from {{ ref('stg_transactions') }}
 where category_type = 'expense'
   and merchant is not null
+  and category_name != 'Transfers'
   and transaction_date >= current_date - interval '28 days'
 group by 1
 order by total_spent desc
